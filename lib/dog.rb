238a16f2b98f9f05f288a1051d2302a8918ce647
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-05 03:31:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def initialize(name) 
+    @name = dogs_name
+  end 
+  
+  def name=(dogs_name)
+    @name = dogs_name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  
+  
+  
+  
+end 
